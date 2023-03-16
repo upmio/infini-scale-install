4f65a71394e3400acd0278c50788569dfa9b91ae
鲍琳 <johnny.beaus@gmail.com>
2023-03-16 17:44:12 +0800

    add child cluster install

--- addons/clusternet/README.md
+++ addons/clusternet/README.md
@@ -1,18 +1,28 @@
-# 部署 clusternet Parnet Cluster
+# 部署 clusternet
 
 本文档目标是指导安装多集群管理程序 [clusternet](https://github.com/clusternet/clusternet) 。
 
 ## 快速安装clusternet Parnet Cluster
 
-### 1. 设置必要的环境变量
+### 1. 确定登录kubernetes 
 
-CLUSTERNET_CONTROLLER_NODE_NAMES：指定安装clusternet controller pod的节点名称，节点名称可以使用","作为分隔符，表示多个节点名称，安装程序会对节点进行label固定安装节点。
+使用 kubectl 确定目前连接的集群是 Parnet Cluster。
+
+```console
+kubectl cluster-info
+```
+
+**注意⚠️：运行在 Parnet Cluster 中的 kube-apiserver 应该配置标志 --aggregator-reject-forwarding-redirect=false。**
+
+### 2. 设置必要的环境变量
+
+CLUSTERNET_CONTROLLER_NODE_NAMES：指定安装clusternet-controller pod的节点名称，节点名称可以使用","作为分隔符，表示多个节点名称，安装程序会对节点进行label固定安装节点。
 
 ```console
 export CLUSTERNET_CONTROLLER_NODE_NAMES="clusternet-control-plan01"
 ```
 
-### 2. 运行安装脚本
+### 3. 运行安装脚本
 
 **注意⚠️：如果找不到 Helm3，将自动安装。**
 
@@ -33,12 +43,11 @@ registrationToken：用于 clusternet-agent 安装时需要连接的注册令牌
 [Info][2023-03-16T16:44:19+0800]: registrationToken=3bbf21.02056c07afc35cc3. PLEASE REMEMBER THIS.
 ```
 
-等待几分钟。 如果所有 clusternet pod 都在运行，则 clusternet 将成功安装。
+等待几分钟。 如果所有 clusternet-controller pod 都在运行，则 clusternet 将成功安装。
 
 ```console
 kubectl get --namespace clusternet-system pods -w
 ```
-
 
 ## 使用 Helm 卸载 Parnet cluster Charts
 
@@ -46,6 +55,61 @@ kubectl get --namespace clusternet-system pods -w
 # Helm
 helm uninstall clusternet-hub --namespace clusternet-system
 helm uninstall clusternet-scheduler --namespace clusternet-system
+```
+
+这将删除与 Charts 关联的所有 Kubernetes 组件并删除发布。
+
+_请参阅 [helm uninstall](https://helm.sh/docs/helm/helm_uninstall/) 获取命令文档。_
+
+## 快速安装clusternet Child Cluster
+
+### 1. 确定登录kubernetes
+
+使用 kubectl 确定目前连接的集群是 Child Cluster。
+
+```console
+kubectl cluster-info
+```
+
+### 2. 设置必要的环境变量
+
+CLUSTERNET_PARENT_URL：连接 Parent cluster 的URL
+CLUSTERNET_REGISTRATION_TOKEN：连接 Parent cluster 的安全令牌
+CLUSTERNET_AGENT_NODE_NAMES：指定安装 clusternet-agent pod 的节点名称，节点名称可以使用","作为分隔符，表示多个节点名称，安装程序会对节点进行label固定安装节点。
+CLUSTERNET_REG_NAME：集群的注册名称
+CLUSTERNET_REG_NAMESPACE：集群的注册namespace
+
+```console
+export CLUSTERNET_PARENT_URL="https://xxxx:6443"
+export CLUSTERNET_REGISTRATION_TOKEN="xxxxxx.xxxxxxxxxxxxxx"
+export CLUSTERNET_AGENT_NODE_NAMES="clusternet-agent01"
+export CLUSTERNET_REG_NAME="mycluster"
+export CLUSTERNET_REG_NAMESPACE="mycluster-ns"
+```
+
+### 3. 运行安装脚本
+
+**注意⚠️：如果找不到 Helm3，将自动安装。**
+
+**注意⚠️：安装脚本会对指定节点进行添加label的操作。**
+
+运行安装脚本
+```console
+# BASH
+curl -sSL https://raw.githubusercontent.com/upmio/infini-scale-install/main/addons/clusternet/install_child_el7.sh | sh -
+```
+
+等待几分钟。 如果所有 clusternet-agent pod 都在运行，则 clusternet 将成功安装。
+
+```console
+kubectl get --namespace clusternet-system pods -w
+```
+
+## 使用 Helm 卸载 Child cluster Charts
+
+```console
+# Helm
+helm uninstall clusternet-agent --namespace clusternet-system
 ```
 
 这将删除与 Charts 关联的所有 Kubernetes 组件并删除发布。
---
+++ addons/clusternet/install_child_el7.sh
@@ -0,0 +1,167 @@
+#!/usr/bin/env bash
+
+# You must be prepared as follows before run install.sh:
+#
+# 1. CLUSTERNET_PARENT_URL MUST be set as environment variable, for an example:
+#
+#        export CLUSTERNET_PARENT_URL="https://xxxx:6443"
+#
+# 2. CLUSTERNET_REGISTRATION_TOKEN MUST be set as environment variable, for an example:
+#
+#        export CLUSTERNET_REGISTRATION_TOKEN="xxxxxx.xxxxxxxxxxxxxx"
+#
+# 3. CLUSTERNET_AGENT_NODE_NAMES MUST be set as environment variable, for an example:
+#
+#        export CLUSTERNET_AGENT_NODE_NAMES="clusternet-agent01"
+#
+# 3. CLUSTERNET_REG_NAME MUST be set as environment variable, for an example:
+#
+#        export CLUSTERNET_REG_NAME="mycluster"
+#
+# 3. CLUSTERNET_REG_NAMESPACE MUST be set as environment variable, for an example:
+#
+#        export CLUSTERNET_REG_NAMESPACE="mycluster-ns"
+#
+
+readonly NAMESPACE="clusternet-system"
+readonly TIME_OUT_SECOND="600s"
+
+INSTALL_LOG_PATH=""
+
+info() {
+  echo "[Info][$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" | tee -a "${INSTALL_LOG_PATH}"
+}
+
+error() {
+  echo "[Error][$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" | tee -a "${INSTALL_LOG_PATH}"
+  exit 1
+}
+
+install_kubectl() {
+  info "Install kubectl..."
+  if ! curl -LOs "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"; then
+    error "Fail to get kubectl, please confirm whether the connection to dl.k8s.io is ok?"
+  fi
+  if ! sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl; then
+    error "Install kubectl fail"
+  fi
+  info "Kubectl install completed"
+}
+
+install_helm() {
+  info "Install helm..."
+  if ! curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3; then
+    error "Fail to get helm installed script, please confirm whether the connection to raw.githubusercontent.com is ok?"
+  fi
+  chmod 700 get_helm.sh
+  if ! ./get_helm.sh; then
+    error "Fail to get helm when running get_helm.sh"
+  fi
+  info "Helm install completed"
+}
+
+install_clusternet_agent() {
+  local release="clusternet-agent"
+  # check if clusternet-hub already installed
+  if helm status ${release} -n ${NAMESPACE} &>/dev/null; then
+    error "${release} already installed. Use helm remove it first"
+  fi
+  info "Install ${release}, It might take a long time..."
+  helm install ${release} clusternet/clusternet-agent \
+    --debug \
+    --namespace ${NAMESPACE} \
+    --create-namespace \
+    --set parentURL="${CLUSTERNET_PARENT_URL}" \
+    --set registrationToken="${REGISTRATION_TOKEN}" \
+    --set replicaCount="${CLUSTERNET_AGENT_NODE_COUNT}" \
+    --set extraArgs.cluster-reg-name="${CLUSTERNET_REG_NAME}" \
+    --set extraArgs.cluster-reg-namespace="${CLUSTERNET_REG_NAMESPACE}" \
+    --set nodeSelector."clusternet\.io/agent"="enable" \
+    --timeout $TIME_OUT_SECOND \
+    --wait 2>&1 | grep "\[debug\]" | awk '{$1="[Helm]"; $2=""; print }' | tee -a "${INSTALL_LOG_PATH}" || {
+    error "Fail to install ${release}."
+  }
+
+  #TODO: check more resources after install
+
+  helm status "${release}" -n "${NAMESPACE}" | grep deployed &>/dev/null || {
+    error "${release} installed fail, check log use helm and kubectl."
+  }
+
+  info "${release} Deployment Completed!"
+}
+
+init_helm_repo() {
+  helm repo add clusternet https://clusternet.github.io/charts &>/dev/null
+  info "Start update helm clusternet repo"
+  if ! helm repo update 2>/dev/null; then
+    error "Helm update clusternet repo error."
+  fi
+}
+
+verify_supported() {
+  local HAS_HELM
+  HAS_HELM="$(type "helm" &>/dev/null && echo true || echo false)"
+  local HAS_KUBECTL
+  HAS_KUBECTL="$(type "kubectl" &>/dev/null && echo true || echo false)"
+  local HAS_CURL
+  HAS_CURL="$(type "curl" &>/dev/null && echo true || echo false)"
+
+  if [[ -z "${CLUSTERNET_PARENT_URL}" ]]; then
+    error "CLUSTERNET_PARENT_URL MUST set in environment variable."
+  fi
+
+  if [[ -z "${CLUSTERNET_REGISTRATION_TOKEN}" ]]; then
+    error "CLUSTERNET_REGISTRATION_TOKEN MUST set in environment variable."
+  fi
+
+  if [[ -z "${CLUSTERNET_AGENT_NODE_NAMES}" ]]; then
+    error "CLUSTERNET_AGENT_NODE_NAMES MUST set in environment variable."
+  fi
+  local control_node_array
+  IFS="," read -r -a control_node_array <<<"${CLUSTERNET_AGENT_NODE_NAMES}"
+  CLUSTERNET_AGENT_NODE_COUNT=0
+  for node in "${control_node_array[@]}"; do
+    kubectl label node "${node}" 'clusternet.io/agent=enable' --overwrite &>/dev/null || {
+      error "kubectl label node ${node} 'clusternet.io/control-plane=enable' failed, use kubectl to check reason"
+    }
+    ((CLUSTERNET_AGENT_NODE_COUNT++))
+  done
+
+  if [[ -z "${CLUSTERNET_REG_NAME}" ]]; then
+    error "REGISTRATION_TOKEN MUST set in environment variable."
+  fi
+
+  if [[ -z "${CLUSTERNET_REG_NAMESPACE}" ]]; then
+    error "REGISTRATION_TOKEN MUST set in environment variable."
+  fi
+
+  if [[ "${HAS_CURL}" != "true" ]]; then
+    error "curl is required"
+  fi
+
+  if [[ "${HAS_HELM}" != "true" ]]; then
+    install_helm
+  fi
+
+  if [[ "${HAS_KUBECTL}" != "true" ]]; then
+    install_kubectl
+  fi
+}
+
+init_log() {
+  INSTALL_LOG_PATH=/tmp/clusternet_install-$(date +'%Y-%m-%d_%H-%M-%S').log
+  if ! touch "${INSTALL_LOG_PATH}"; then
+    error "Create log file ${INSTALL_LOG_PATH} error"
+  fi
+  info "Log file create in path ${INSTALL_LOG_PATH}"
+}
+
+main() {
+  init_log
+  verify_supported
+  init_helm_repo
+  install_clusternet_agent
+}
+
+main

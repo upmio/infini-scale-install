fb4250d7a740f5c9ca63042e77268840e873bebc
鲍琳 <johnny.beaus@gmail.com>
2023-03-16 13:39:12 +0800

    add clusternet

---
+++ addons/clusternet/README.md
@@ -0,0 +1,48 @@
+# 部署 clusternet Parnet Cluster
+
+本文档目标是指导安装多集群管理程序 [clusternet](https://github.com/clusternet/clusternet) 。
+
+## 快速安装clusternet Parnet Cluster
+
+### 1. 设置必要的环境变量
+
+CONTROLLER_NODE_NAMES：指定安装controller pod的节点名称，节点名称可以使用","作为分隔符，表示多个节点名称，安装程序会对节点进行label固定安装节点。
+
+```console
+export CONTROLLER_NODE_NAMES="clusternet-control-plan01"
+```
+
+### 2. 运行安装脚本
+
+**注意⚠️：如果找不到 Helm3，将自动安装。**
+
+**注意⚠️：安装脚本会对指定节点进行添加label的操作。**
+
+运行安装脚本
+```console
+# BASH
+curl -sSL https://raw.githubusercontent.com/upmio/infini-scale-install/main/addons/clusternet/install_parnet_sel7.sh | sh -
+```
+
+等几分钟。 如果所有 carina  pod 都在运行，则 carina 将成功安装。
+
+```console
+kubectl get --namespace clusternet-system pods -w
+```
+
+## 使用 Helm 卸载 Parnet cluster Charts
+
+```console
+# Helm
+helm uninstall clusternet-hub --namespace clusternet-system
+helm uninstall clusternet-scheduler --namespace clusternet-system
+```
+
+这将删除与 Charts 关联的所有 Kubernetes 组件并删除发布。
+
+_请参阅 [helm uninstall](https://helm.sh/docs/helm/helm_uninstall/) 获取命令文档。_
+
+## License
+
+<!-- Keep full URL links to repo files because this README syncs from main to gh-pages.  -->
+[Apache 2.0 License](https://raw.githubusercontent.com/upmio/infini-scale-install/main/LICENSE).
---
+++ addons/clusternet/install_parnet_el7.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env bash
+
+# You must be prepared as follows before run install.sh:
+#
+# 1. CONTROLLER_NODE_NAMES MUST be set as environment variable, for an example:
+#
+#        export CONTROLLER_NODE_NAMES="master01,master02"
+#
+
+readonly NAMESPACE="clusternet-system"
+readonly TIME_OUT_SECOND="600s"
+
+INSTALL_LOG_PATH=""
+
+info() {
+  echo "[Info][$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" | tee -a "${INSTALL_LOG_PATH}"
+}
+
+error() {
+  echo "[Error][$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" | tee -a "${INSTALL_LOG_PATH}"
+  exit 1
+}
+
+install_kubectl() {
+  info "Install kubectl..."
+  if ! curl -LOs "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"; then
+    error "Fail to get kubectl, please confirm whether the connection to dl.k8s.io is ok?"
+  fi
+  if ! sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl; then
+    error "Install kubectl fail"
+  fi
+  info "Kubectl install completed"
+}
+
+install_helm() {
+  info "Install helm..."
+  if ! curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3; then
+    error "Fail to get helm installed script, please confirm whether the connection to raw.githubusercontent.com is ok?"
+  fi
+  chmod 700 get_helm.sh
+  if ! ./get_helm.sh; then
+    error "Fail to get helm when running get_helm.sh"
+  fi
+  info "Helm install completed"
+}
+
+install_clusternet_hub() {
+  local release="clusternet-hub"
+  # check if clusternet-hub already installed
+  if helm status ${release} -n ${NAMESPACE} &>/dev/null; then
+    error "${release} already installed. Use helm remove it first"
+  fi
+  info "Install ${release}, It might take a long time..."
+  helm install ${release} clusternet/clusternet-hub \
+    --debug \
+    --namespace ${NAMESPACE} \
+    --create-namespace \
+    --set replicaCount=${CONTROLLER_NDOE_COUNT} \
+    --set nodeSelector."clusternet\.io/control-plane"="enable" \
+    --timeout $TIME_OUT_SECOND \
+    --wait 2>&1 | grep "\[debug\]" | awk '{$1="[Helm]"; $2=""; print }' | tee -a "${INSTALL_LOG_PATH}" || {
+    error "Fail to install ${RELEASE}."
+  }
+
+  #TODO: check more resources after install
+
+  helm status "${release}" -n "${NAMESPACE}" | grep deployed &>/dev/null || {
+    error "${release} installed fail, check log use helm and kubectl."
+  }
+
+  info "${release} Deployment Completed!"
+}
+
+install_clusternet_scheduler() {
+  local release="clusternet-scheduler"
+  # check if clusternet-scheduler already installed
+  if helm status ${release} -n ${NAMESPACE} &>/dev/null; then
+    error "${release} already installed. Use helm remove it first"
+  fi
+  info "Install ${release}, It might take a long time..."
+  helm install ${release} clusternet/clusternet-scheduler \
+    --debug \
+    --namespace ${NAMESPACE} \
+    --create-namespace \
+    --set replicaCount=${CONTROLLER_NDOE_COUNT} \
+    --set nodeSelector."clusternet\.io/control-plane"="enable" \
+    --timeout $TIME_OUT_SECOND \
+    --wait 2>&1 | grep "\[debug\]" | awk '{$1="[Helm]"; $2=""; print }' | tee -a "${INSTALL_LOG_PATH}" || {
+    error "Fail to install ${RELEASE}."
+  }
+
+  #TODO: check more resources after install
+
+  helm status "${release}" -n "${NAMESPACE}" | grep deployed &>/dev/null || {
+    error "${release} installed fail, check log use helm and kubectl."
+  }
+
+  info "${release} Deployment Completed!"
+}
+
+init_helm_repo() {
+  helm repo add clusternet https://clusternet.github.io/charts &>/dev/null
+  info "Start update helm clusternet repo"
+  if ! helm repo update 2>/dev/null; then
+    error "Helm update clusternet repo error."
+  fi
+}
+
+verify_supported() {
+  local HAS_HELM
+  HAS_HELM="$(type "helm" &>/dev/null && echo true || echo false)"
+  local HAS_KUBECTL
+  HAS_KUBECTL="$(type "kubectl" &>/dev/null && echo true || echo false)"
+  local HAS_CURL
+  HAS_CURL="$(type "curl" &>/dev/null && echo true || echo false)"
+
+  if [[ -z "${CONTROLLER_NODE_NAMES}" ]]; then
+    error "CONTROLLER_NODE_NAMES MUST set in environment variable."
+  fi
+
+  local control_node_array
+  IFS="," read -r -a control_node_array <<<"${CONTROLLER_NODE_NAMES}"
+  CONTROLLER_NDOE_COUNT=0
+  for node in "${control_node_array[@]}"; do
+    kubectl label node "${node}" 'clusternet.io/control-plane=enable' --overwrite &>/dev/null || {
+      error "kubectl label node ${node} 'clusternet.io/control-plane=enable' failed, use kubectl to check reason"
+    }
+    ((CONTROLLER_NDOE_COUNT++))
+  done
+
+  if [[ "${HAS_CURL}" != "true" ]]; then
+    error "curl is required"
+  fi
+
+  if [[ "${HAS_HELM}" != "true" ]]; then
+    install_helm
+  fi
+
+  if [[ "${HAS_KUBECTL}" != "true" ]]; then
+    install_kubectl
+  fi
+}
+
+init_log() {
+  INSTALL_LOG_PATH=/tmp/clusternet_install-$(date +'%Y-%m-%d_%H-%M-%S').log
+  if ! touch "${INSTALL_LOG_PATH}"; then
+    error "Create log file ${INSTALL_LOG_PATH} error"
+  fi
+  info "Log file create in path ${INSTALL_LOG_PATH}"
+}
+
+############################################
+# Check if helm release deployment correctly
+# Arguments:
+#   release
+#   namespace
+############################################
+verify_installed() {
+
+}
+
+main() {
+  init_log
+  verify_supported
+  init_helm_repo
+  install_clusternet_hub
+  install_clusternet_scheduler
+}
+
+main
